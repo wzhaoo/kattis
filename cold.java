2fb8b4b97279eeb8c93ae593e162c3a9e74d3ed8
wzhaoo <60197469+wzhaoo@users.noreply.github.com>
2020-05-08 18:14:41 -0400

    Update and rename cold to cold.java

--- cold.java
+++ cold.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 
-public class coldputer{
+public class cold{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int count = 0;
